edccfb46fc4b75195cd3e8fe323d2e6e214a1a66
Jelani Sanders <jelanisands@gmail.com>
2023-06-19 01:02:59 -0400

    initial commit

---
+++ migrations/01_schema.sql
@@ -0,0 +1,19 @@
+INSERT INTO users (name, email, password)
+
+
+
+
+
+
+
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+
+
+
+
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+
+
+
+
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
